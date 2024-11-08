d074af1c474d96fb0dee4823581d8eaaefd1a014
sharunraj <sharunraj39@gmail.com>
2024-11-08 09:51:56 +0530

    changed from rest to web client

--- src/main/java/com/example/fitness_service/FitnessServiceApplication.java
+++ src/main/java/com/example/fitness_service/FitnessServiceApplication.java
@@ -3,6 +3,8 @@ package com.example.fitness_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +14,8 @@ public class FitnessServiceApplication {
 		SpringApplication.run(FitnessServiceApplication.class, args);
 	}
 
-
+	@Bean
+	WebClient.Builder webClientBuilder() {
+		return WebClient.builder();
+	}
 }
